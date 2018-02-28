4fd089eaa11abd9f941aa3ccad415f86287bbb52
Oskar <Oskar@130.240.92.81>
2018-02-28 13:43:47 +0100

    Lagt till vad jag tror behövs i execute för alla events

--- src/supermarket/event/ArrivalEvent.java
+++ src/supermarket/event/ArrivalEvent.java
@@ -1,9 +1,20 @@
 package supermarket.event;
 import sim.Event;
+import sim.SimulatorStateADT;
+import sim.SortedSequence;
 /**
  * Event som körs då en kund anländer.
  *
  */
 public class ArrivalEvent extends Event {
+
+	@Override
+	public void execute(SortedSequence seq, SimulatorStateADT state) {
+		seq.sortEventQueue(this);
+		state.notifyObservers();
+		state.hasChanged();
+		
+		
+	}
 
 }
--- src/supermarket/event/StartEvent.java
+++ src/supermarket/event/StartEvent.java
@@ -1,9 +1,23 @@
 package supermarket.event;
 import sim.Event;
+import sim.SimulatorStateADT;
+import sim.SortedSequence;
+import supermarket.FIFO;
 /**
  * Event som körs då simuleringen startar.
  *
  */
 public class StartEvent extends Event {
+
+	@Override
+	public void execute(SortedSequence seq, SimulatorStateADT state) {
+		
+		
+		seq.sortEventQueue(this);
+		state.notifyObservers();
+		state.hasChanged();
+		
+		
+	}
 
 }
