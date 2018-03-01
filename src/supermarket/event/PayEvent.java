2c3e7cfde2f88e35a65df9fa8e61bd940b2f57ba
kottz <edward.kallstedt@gmail.com>
2018-03-01 14:58:28 +0100

    payEvent

--- src/supermarket/event/PayEvent.java
+++ src/supermarket/event/PayEvent.java
@@ -16,14 +16,21 @@ public class PayEvent extends Event {
 	private ShoppingState state;
 	private FIFO q = state.queue;
 	
-	public void execute(SortedSequence seq, ShoppingState state) {
-		
-	}
-	
 	public PayEvent(SortedSequence seq, ShoppingState state, Customer c, double time) {
 		this.c = c;
 		this.state = state;
 		this.time = time;
 	}
-
+	
+	public void execute(SortedSequence seq, ShoppingState state) {
+	
+		if(state.freeCheckout()) {
+			state.idleCheckouts--;
+			double newTime = time + state.calculateCheckoutTime();
+			Event leave = new LeaveEvent(seq,state,c,newTime); //Skickar vidare denna kund till leave, newTime = tiden då betalningen är klar.
+			seq.sortEventQueue(leave); //Lägger till kund och sorterar
+		} else {
+			q.add(c);
+		}
+	}
 }
